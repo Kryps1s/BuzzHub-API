fb47ee22a9cabe7d258355a13623b7d2aa6e5ea5
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-06-26 10:14:48 -0400

    added limit and type filtering for get all resolver

--- getAllEvents.py
+++ getAllEvents.py
@@ -4,11 +4,16 @@ TABLE_NAME = "Events-dev"
 
 
 def lambda_handler(event, context):
-    # Create a DynamoDB client for the specified region
     dynamodb = boto3.client('dynamodb', region_name='ca-central-1')
-
     response = dynamodb.scan(TableName=TABLE_NAME)
     items = response.get('Items', [])
     deserializer = TypeDeserializer()
     items = [{key: deserializer.deserialize(value) for key, value in item.items()} for item in items]
+    if 'arguments' in event[0]:
+        arguments = event[0]['arguments']
+        if 'type' in arguments:
+            items = [item for item in items if item.get('type') == arguments['type']]
+        if 'limit' in arguments:
+            limit = arguments.get('limit')
+            items = items[:limit]
     return [items]
