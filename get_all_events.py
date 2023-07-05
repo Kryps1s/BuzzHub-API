b9d17b670a60337382e5d3738cfd128075fe93c2
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-05 14:28:10 -0400

    linted

--- get_all_events.py
+++ get_all_events.py
@@ -1,3 +1,6 @@
+"""
+This lambda function will return all events in the calendar, with optional modifiers. 
+"""
 import os
 import boto3
 from boto3.dynamodb.types import TypeDeserializer
@@ -6,12 +9,23 @@ TABLE_NAME = os.environ['env']+"_calendar"
 SORT_NAME = "upcomingEvent"
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, _):
+    """
+    wrapper aroud the DynamoDB scan function. 
+    event : the event object from the GraphQL query
+    arguments : the arguments object from the GraphQL query
+        type : string, from the GraphQL type enum
+        limit : int, the number of items to return
+    """
     dynamodb = boto3.client('dynamodb', region_name=os.environ['region'])
     response = dynamodb.scan(TableName=TABLE_NAME, IndexName=SORT_NAME)
     items = response.get('Items', [])
     deserializer = TypeDeserializer()
-    items = [{key: deserializer.deserialize(value) for key, value in item.items()} for item in items]
+    items = [
+        {
+            key: deserializer.deserialize(value)
+            for key, value in item.items()
+        } for item in items]
     if 'arguments' in event:
         arguments = event['arguments']
         if 'type' in arguments:
