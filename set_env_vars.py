c2ebe004dfeb6e4e3179c19452f7d3b88a21d4d3
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-07 15:52:25 -0400

    setting env vars

---
+++ set_env_vars.py
@@ -0,0 +1,12 @@
+"""for github actions to set environment variables"""
+import os
+
+try:
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, "a", encoding="utf-8") as env_file:
+        env_file.write("env=test")
+        env_file.write("region=ca-central-1")
+    print("+++ Environment variables is set +++")
+
+except FileNotFoundError as e:
+    print(f"+++ Error while setting the environment variable :: {e} +++")
