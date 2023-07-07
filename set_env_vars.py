fe9888f428571943bdd96e89365dfed14eb76619
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-07 15:57:59 -0400

    setting env vars

--- set_env_vars.py
+++ set_env_vars.py
@@ -4,9 +4,7 @@ import os
 try:
     env_file = os.getenv('GITHUB_ENV')
     with open(env_file, "a", encoding="utf-8") as env_file:
-        env_file.write("env=test")
-    with open(env_file, "a", encoding="utf-8") as env_file:
-        env_file.write("region=ca-central-1")
+        env_file.write("env=test\nregion=ca-central-1")
     print("+++ Environment variables is set +++")
 
 except FileNotFoundError as e:
