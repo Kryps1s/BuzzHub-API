39208505aa22b7f71fda73442541b0ca38f96488
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-13 16:43:14 -0400

    en vars

--- set_env_vars.py
+++ set_env_vars.py
@@ -4,18 +4,17 @@ import os
 try:
     env_file = os.getenv('GITHUB_ENV')
     with open(env_file, "a", encoding="utf-8") as env_file:
-        env_file.write("env=test\n\
-                       region=ca-central-1\n\
-                       TRELLO_BOARD_BEEKEEPING=tVNzQnNQ\n\
-                       TRELLO_BOARD_COLLECTIVE=RRqKnGAA\n\
-                       TRELLO_BOARD_MEETING=KH88ovyS\n\
-                       TRELLO_BOARD_TEMPLATES=KH88ovyS\n\
-                       BEEKEEPING_BOARD_ID=61f889ff737a1d7b1031bb9d\n\
-                       MEETING_BOARD_ID=61f889ff737a1d7b1031bb9d\n\
-                       COLLECTIVE_BOARD_ID=61f889ff737a1d7b1031bb9d\n\
-                       TRELLO_KEY=mocked\n\
-                       TRELLO_TOKEN=mocked")
-    print("+++ Environment variables is set +++")
-
+        env_file.write("env=test\n")
+        env_file.write("region=ca-central-1\n")
+        env_file.write("TRELLO_BOARD_BEEKEEPING=tVNzQnNQ\n")
+        env_file.write("TRELLO_BOARD_COLLECTIVE=RRqKnGAA\n")
+        env_file.write("TRELLO_BOARD_MEETING=KH88ovyS\n")
+        env_file.write("TRELLO_BOARD_TEMPLATES=KH88ovyS\n")
+        env_file.write("BEEKEEPING_BOARD_ID=61f889ff737a1d7b1031bb9d\n")
+        env_file.write("MEETING_BOARD_ID=61f889ff737a1d7b1031bb9d\n")
+        env_file.write("COLLECTIVE_BOARD_ID=61f889ff737a1d7b1031bb9d\n")
+        env_file.write("TRELLO_KEY=mocked\n")
+        env_file.write("TRELLO_TOKEN=mocked\n")
+    print("+++ Environment variables are set +++")
 except FileNotFoundError as e:
-    print(f"+++ Error while setting the environment variable :: {e} +++")
+    print(f"+++ Error while setting the environment variable: {e} +++")
