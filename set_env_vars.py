c3b18094a5b8b4d88407bbdc17d97fae7427e6da
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-13 16:29:27 -0400

    mocked key and token

--- set_env_vars.py
+++ set_env_vars.py
@@ -12,7 +12,9 @@ try:
                        TRELLO_BOARD_TEMPLATES=KH88ovyS\n \
                        BEEKEEPING_BOARD_ID=61f889ff737a1d7b1031bb9d\n \
                        MEETING_BOARD_ID=61f889ff737a1d7b1031bb9d\n \
-                       COLLECTIVE_BOARD_ID=61f889ff737a1d7b1031bb9d\n")
+                       COLLECTIVE_BOARD_ID=61f889ff737a1d7b1031bb9d\n \
+                       TRELLO_KEY=mocked\n \
+                       TRELLO_TOKEN=mocked")
     print("+++ Environment variables is set +++")
 
 except FileNotFoundError as e:
