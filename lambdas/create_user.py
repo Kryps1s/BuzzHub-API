38d51ce649858c69af6341dadf311c6ff030dbcf
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-18 19:41:22 -0400

    changes to workflows, added create user and login

---
+++ lambdas/create_user.py
@@ -0,0 +1,98 @@
+"""Create a new user in the cognito user pool."""
+import os
+import re
+import boto3
+import requests
+from botocore.exceptions import ClientError
+
+class TrelloAPIError(Exception):
+    """Exception raised for errors in the Trello API"""
+
+def fetch_members():
+    """Fetch all members from the organization"""
+    url = "https://api.trello.com/1/organizations/" + os.environ['TRELLO_ORGANIZATION'] + "/members"
+    headers = {
+    "Accept": "application/json"
+    }
+    query = {
+    'key': os.environ['TRELLO_KEY'],
+    'token': os.environ['TRELLO_TOKEN']
+    }
+    response = requests.request(
+    "GET",
+    url,
+    headers=headers,
+    params=query,
+    timeout=30
+    )
+    if response.ok is False:
+        raise TrelloAPIError("Trello API error: " + response['error'])
+    #filter invalid members from list of invalid members
+
+    invalid_members = ["585a7e82c8a3142c77cfb22e", "placeholder_buzzhub_user"]
+    members = [user['id'] for user in response.json() if user['id'] not in invalid_members]
+    return members
+
+def validate_user(user):
+    """Validate the user"""
+    #check input has all fields in the user class
+    error = None
+    #pylint: disable=R0916
+    if "first_name" not in user or "last_name" not in user or "email" not in user \
+    or "password" not in user or "trello_id" not in user or "code" not in user:
+        error = {"error": "Missing field"}
+    #check first and last name are at least 3 characters
+    elif len(user["first_name"]) < 3 or len(user["last_name"]) < 3:
+        error = {"error": "First and last name must be at least 3 characters"}
+    #check email is valid regex
+    elif not re.match(r"[^@]+@[^@]+\.[^@]+", user["email"]):
+        error = {"error": "Invalid email"}
+    #check password is at least 8 characters, and contains at least one number,
+    # one uppercase letter, and one lowercase letter, and one special character
+    elif not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}",
+                     user["password"]):
+        #pylint: disable=C0301
+        error = {"error": "Password must be at least 8 characters, and contain at least one number, one uppercase letter, and one lowercase letter, and one special character"}
+    #check trello id is valid
+    elif user["trello_id"] not in fetch_members():
+        error = {"error": "Invalid trello id"}
+    elif user["code"] != os.environ['BUZZHUB_ACCESS_CODE']:
+        error = {"error": "Invalid access code"}
+    return error
+
+def create_user(user):
+    """Create the user in the cognito user pool"""
+    try:
+        client = boto3.client('cognito-idp')
+        client.sign_up(
+            ClientId= os.environ['COGNITO_CLIENT_ID'],
+            Username=user["email"],
+            Password=user["password"],
+            UserAttributes=[
+                {
+                    'Name': 'name',
+                    'Value': user["first_name"] + " " + user["last_name"]
+                },
+                {
+                    'Name': 'custom:trello',
+                    'Value': user["trello_id"]
+                }
+            ]
+        )
+        #print to cloudwatch logs
+        print("user created" + user["email"])
+        return user["email"]
+    except ClientError as err:
+        print(err)
+        return {"error": str(err)}
+
+def lambda_handler(event, _):
+    """Lambda handler"""
+    #get the user from the event
+    #validate the user
+    user = event["arguments"]
+    error = validate_user(user)
+    if error:
+        print(error)
+        return error
+    return create_user(user)
---
+++ tests/test_create_users.py
@@ -0,0 +1,115 @@
+"""Tests for create_users"""
+from unittest.mock import patch
+import pytest
+from lambdas.create_user import lambda_handler
+
+
+@pytest.fixture(name='event')
+def fixture_event():
+    """ Generates Appsync GraphQL Event"""
+    return {
+        "arguments": {
+            "email": "elliot@test.com",
+            "password": "Password!2",
+            "code" : "test",
+            "trello_id": "elliot",
+            "first_name": "Elliot",
+            "last_name": "Alderson"
+
+        }
+    }
+
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users(mock_create_user,mock_fetch_members, event):
+    """ Test create_users """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result == 'user created'
+
+#test for missing email
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_no_email(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when no email is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    del event['arguments']['email']
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'Missing field'
+
+#test for invalid password
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_password(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid password is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['password'] = "Password"
+    result = lambda_handler(event, {})
+    # Check the result
+    #pylint: disable=line-too-long
+    assert result['error'] == 'Password must be at least 8 characters, and contain at least one number, one uppercase letter, and one lowercase letter, and one special character'
+
+#test for invalid code
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_code(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid code is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['code'] = "test1"
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'Invalid access code'
+
+#test for invalid trello id
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_trello_id(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid trello id is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['trello_id'] = "elliot1"
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'Invalid trello id'
+
+#test for invalid first name
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_first_name(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid first name is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['first_name'] = "El"
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'First and last name must be at least 3 characters'
+
+#test for invalid last name
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_last_name(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid last name is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['last_name'] = "Al"
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'First and last name must be at least 3 characters'
+
+#test for invalid email
+@patch('lambdas.create_user.fetch_members')
+@patch('lambdas.create_user.create_user')
+def test_create_users_fail_invalid_email(mock_create_user,mock_fetch_members, event):
+    """ Test create_users fails when invalid email is provided """
+    mock_create_user.return_value = "user created"
+    mock_fetch_members.return_value = ["elliot"]
+    event['arguments']['email'] = "elliot@test"
+    result = lambda_handler(event, {})
+    # Check the result
+    assert result['error'] == 'Invalid email'
